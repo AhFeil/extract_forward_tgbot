bed729e49c70b6eb164bdcf9b2a128ed56a8940d
AhFeil <vgamebox@outlook.com>
2023-07-31 09:12:41 +0800

    add parameter

--- try1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def Find(string):
-    # findall() 查找匹配正则表达式的字符串
-    url = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', string)
-    return url
-
-string = 'Runoob 的网页地址为：https://www.runoob.com，Google 的网页地址为：https://www.google.com'
-print("Urls: ", Find(string))
