e2ade45c65fca2e7ba6f757b1c8135d7705059cb
AhFeil <vgamebox@outlook.com>
2023-10-10 02:03:38 +0800

    await under main not right

--- test_pillow.py
+++ test_pillow.py
@@ -1,5 +1,6 @@
 import os.path
 import glob
+import asyncio
 
 from PIL import Image
 
@@ -8,8 +9,8 @@ from process_images import generate_gif, merge_multi_images, add_text, open_imag
 
 # 指定图片文件夹路径
 # folder_path = 'images'   # 生成 GIF 的图片例子
-# folder_path = os.path.join('images', 'text_images')   # 补充字的的图片例子
-folder_path = os.path.join('images', 'merge_images')   # 合并的图片例子
+folder_path = os.path.join('images', 'text_images')   # 补充字的的图片例子
+# folder_path = os.path.join('images', 'merge_images')   # 合并的图片例子
 
 # 获取所有图片文件名，包括jpg, png格式的图片
 image_files = glob.glob(os.path.join(folder_path, "*.[jJ][pP][gG]")) \
@@ -21,10 +22,17 @@ image_files = glob.glob(os.path.join(folder_path, "*.[jJ][pP][gG]")) \
 # 网络图片
 # image_files = ["https://ib.ahfei.blog:443/imagesbed/202309052211621-23-09-56.png"]
 
-img_list = await open_image_from_various(image_files)
+### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### ---
+
+# await 关键字不能在 Python 的主模块中直接使用。如果你想在代码的最顶层（即非函数或方法内部），需要使用事件循环。例如，用 asyncio 提供的事件循环
+# 创建一个事件循环
+loop = asyncio.get_event_loop()
+# 使用事件循环运行你的异步函数
+img_list = loop.run_until_complete(open_image_from_various(image_files))
+
 duration_time = 3000
 middle_interval = 10
-text = "示例文字"
+text = "#ll示例文字"
 
 # 测试
 # GIF
@@ -34,9 +42,11 @@ text = "示例文字"
 #     f.write(gif_io.read())
 
 # 合并
-gif_io = merge_multi_images(img_list, middle_interval)
+# gif_io = merge_multi_images(img_list, middle_interval)
 # 添加文本
-# gif_io = add_text(img_list, text)
+gif_io = add_text(img_list, text)
+
+
 
 # 关闭图像文件
 for img in img_list:
