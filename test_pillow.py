d59cf84a2ed8f6084033f263135ef6dc34cb27ca
AhFeil <vgamebox@outlook.com>
2023-10-23 17:42:05 +0800

    use transpose to reuse codes, height and width is same now

--- test_pillow.py
+++ test_pillow.py
@@ -9,8 +9,8 @@ from process_images import generate_gif, merge_multi_images, add_text, open_imag
 
 # 指定图片文件夹路径
 # folder_path = 'images'   # 生成 GIF 的图片例子
-folder_path = os.path.join('images', 'text_images')   # 补充字的的图片例子
-# folder_path = os.path.join('images', 'merge_images')   # 合并的图片例子
+# folder_path = os.path.join('images', 'text_images')   # 补充字的的图片例子
+folder_path = os.path.join('images', 'merge_images')   # 合并的图片例子
 
 # 获取所有图片文件名，包括jpg, png格式的图片
 image_files = glob.glob(os.path.join(folder_path, "*.[jJ][pP][gG]")) \
@@ -22,7 +22,6 @@ image_files = glob.glob(os.path.join(folder_path, "*.[jJ][pP][gG]")) \
 # 网络图片
 # image_files = ["https://ib.ahfei.blog:443/imagesbed/202309052211621-23-09-56.png"]
 
-### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### --- ### ---
 
 # await 关键字不能在 Python 的主模块中直接使用。如果你想在代码的最顶层（即非函数或方法内部），需要使用事件循环。例如，用 asyncio 提供的事件循环
 # 创建一个事件循环
@@ -35,25 +34,26 @@ middle_interval = 10
 text = "#ll示例文字"
 
 # 测试（这里用 if）
-# GIF
-# gif_io = generate_gif(img_list, duration_time)
-# # 保存到文件
-# with open('my_gif.gif', 'wb') as f:
-#     f.write(gif_io.read())
-
-# 合并
-# gif_io = merge_multi_images(img_list, middle_interval)
-# 添加文本
-gif_io = add_text(img_list, text)
-
-
+if len(img_list) == 1:   # 添加文本
+    gif_io = add_text(img_list, text)
+    with Image.open(gif_io) as new_image:
+        new_image.show()
+elif len(img_list) in {2, 3, 4}:   # 合并
+    gif_io = merge_multi_images(img_list, middle_interval)
+    with Image.open(gif_io) as new_image:
+        new_image.show()
+elif len(img_list) > 4:   # GIF
+    gif_io = generate_gif(img_list, duration_time)
+    # 保存到文件
+    with open('my_gif.gif', 'wb') as f:
+        f.write(gif_io.read())
+else:
+    gif_io = None
+    print("check test")
 
 # 关闭图像文件
 for img in img_list:
     img.close()
-
-with Image.open(gif_io) as new_image:
-    new_image.show()
 
 gif_io = None
 
